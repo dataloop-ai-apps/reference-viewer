0aef0edde75c46e0c709014f711fdeb243937d3a
Amirzhan Aliyev <amorussr@ya.ru>
2023-01-04 16:08:30 +0600

    feat: add vite-svg-loader and update svg images

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl"
+import viteBasicSslPlugin from '@vitejs/plugin-basic-ssl'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,13 +9,13 @@ export default defineConfig({
     port: 3002,
     https: true,
     proxy: {
-      "/api": {
-        target: "https://gate.dataloop.ai/api",
+      '/api': {
+        target: 'https://gate.dataloop.ai/api',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-  plugins: [vue(), viteBasicSslPlugin()]
+  plugins: [vue(), viteBasicSslPlugin(), svgLoader()],
 })
