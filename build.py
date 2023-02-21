a383e804fe0b9b8b55c18edcd5831aff45ebb150
Aharon Louzon <aharon@dataloop.ai>
2023-02-21 19:07:32 +0200

    old build

--- build.py
+++ build.py
@@ -2,28 +2,17 @@ import shutil
 import json
 import os
  
+with open('./dataloop.json') as json_file:
+    data = json.load(json_file)
+    panel_name = data['components']['panels'][0]['name']
 
-import time
-time.sleep(1000)
+index_path = f'./panels/{panel_name}/index.html'
+lines = []
+with open(index_path, 'r') as html_file:
+    for line in html_file.readlines():
+        if "/assets/" in line:
+            line = line.replace('/assets/', f'../{panel_name}/assets/')
+        lines.append(line)
 
-# print('Starting build process...')
-# print('working directory: ', os.getcwd())
-
-# with open('./dataloop.json') as json_file:
-#     data = json.load(json_file)
-#     panel_name = data['components']['panels'][0]['name']
-
-# print('Got panel name: ', panel_name)
-# index_path = f'./panels/{panel_name}/index.html'
-# print('Got index path: ', index_path)
-
-
-# lines = []
-# with open(index_path, 'r') as html_file:
-#     for line in html_file.readlines():
-#         if "/assets/" in line:
-#             line = line.replace('/assets/', f'../{panel_name}/assets/')
-#         lines.append(line)
-
-# with open(f'./panels/{panel_name}/index.html', 'w') as updated_html_file:
-#     updated_html_file.writelines(lines)
+with open(f'./panels/{panel_name}/index.html', 'w') as updated_html_file:
+    updated_html_file.writelines(lines)
--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# if test -d ./dist; then rm -r ./dist; fi
-# if test -d ./panels/referenceViewer; then rm -r ./panels/referenceViewer; fi
-# npm i
-# npm run build
-# cp -r ./dist ./panels/referenceViewer
+if test -d ./dist; then rm -r ./dist; fi
+if test -d ./panels/referenceViewer; then rm -r ./panels/referenceViewer; fi
+npm i
+npm run build
+cp -r ./dist ./panels/referenceViewer
 
-# ls ./panels/
-# ls ./panels/referenceViewer
+ls ./panels/
+ls ./panels/referenceViewer
 
 python3 ./build.py
 rm -r ./dist
