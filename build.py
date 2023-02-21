366a5b838e7c6d0f5a7aff35e89d5c8f19adf921
Aharon Louzon <aharon@dataloop.ai>
2023-02-21 18:41:42 +0200

    sleep

--- build.py
+++ build.py
@@ -3,24 +3,27 @@ import json
 import os
  
 
-print('Starting build process...')
-print('working directory: ', os.getcwd())
+import time
+time.sleep(1000)
 
-with open('./dataloop.json') as json_file:
-    data = json.load(json_file)
-    panel_name = data['components']['panels'][0]['name']
+# print('Starting build process...')
+# print('working directory: ', os.getcwd())
 
-print('Got panel name: ', panel_name)
-index_path = f'./panels/{panel_name}/index.html'
-print('Got index path: ', index_path)
+# with open('./dataloop.json') as json_file:
+#     data = json.load(json_file)
+#     panel_name = data['components']['panels'][0]['name']
+
+# print('Got panel name: ', panel_name)
+# index_path = f'./panels/{panel_name}/index.html'
+# print('Got index path: ', index_path)
 
 
-lines = []
-with open(index_path, 'r') as html_file:
-    for line in html_file.readlines():
-        if "/assets/" in line:
-            line = line.replace('/assets/', f'../{panel_name}/assets/')
-        lines.append(line)
+# lines = []
+# with open(index_path, 'r') as html_file:
+#     for line in html_file.readlines():
+#         if "/assets/" in line:
+#             line = line.replace('/assets/', f'../{panel_name}/assets/')
+#         lines.append(line)
 
-with open(f'./panels/{panel_name}/index.html', 'w') as updated_html_file:
-    updated_html_file.writelines(lines)
+# with open(f'./panels/{panel_name}/index.html', 'w') as updated_html_file:
+#     updated_html_file.writelines(lines)
