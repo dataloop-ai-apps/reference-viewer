313356ab09b58e7a24ad0dc7206103c4bdb57347
Amirzhan Aliyev <letavocado@proton.me>
2023-05-18 01:31:43 +0600

    build: add tailwindcss and 'Ubuntu' font

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
